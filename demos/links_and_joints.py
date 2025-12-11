11e4abe64e801313f27e100b67f29b2d083ef85e
robobe <robobe2020@gmail.com>
2025-12-11 08:13:36 +0000

    joints

---
+++ demos/links_and_joints.py
@@ -0,0 +1,47 @@
+import pybullet as p
+import pybullet_data
+
+# setup
+p.connect(p.DIRECT)
+p.resetSimulation()
+p.setGravity(gravX=0, gravY=0, gravZ=-9.8)
+p.setAdditionalSearchPath(path=pybullet_data.getDataPath())
+
+# load URDF
+robo = p.loadURDF(
+    "franka_panda/panda.urdf", basePosition=[1, 0, 0.1], useFixedBase=True
+)
+
+# joints
+dic_info = {
+    0: "joint Index",  # starts at 0
+    1: "joint Name",
+    2: "joint Type",  # 0=revolute (rotational), 1=prismatic (sliding), 4=fixed
+    3: "state vectorIndex",
+    4: "velocity vectorIndex",
+    5: "flags",  # nvm always 0
+    6: "joint Damping",
+    7: "joint Friction",  # coefficient
+    8: "joint lowerLimit",  # min angle
+    9: "joint upperLimit",  # max angle
+    10: "joint maxForce",  # max force allowed
+    11: "joint maxVelocity",  # max speed
+    12: "link Name",  # child link connected to this joint
+    13: "joint Axis",
+    14: "parent FramePos",  # position
+    15: "parent FrameOrn",  # orientation
+    16: "parent Index",  # −1 = base
+}
+for i in range(p.getNumJoints(bodyUniqueId=robo)):
+    joint_info = p.getJointInfo(bodyUniqueId=robo, jointIndex=i)
+    print(f"--- JOINT {i} ---")
+    print({dic_info[k]: joint_info[k] for k in dic_info.keys()})
+
+# links
+for i in range(p.getNumJoints(robo)):
+    link_name = p.getJointInfo(robo, i)[12].decode("utf-8")  # field 12="link Name"
+    dyn = p.getDynamicsInfo(robo, i)
+    pos, orn, *_ = p.getLinkState(robo, i)
+    dic_info = {"Mass": dyn[0], "Friction": dyn[1], "Position": pos, "Orientation": orn}
+    print(f"--- LINK {i}: {link_name} ---")
+    print(dic_info)
---
+++ demos/rrbot-velocity.py
@@ -0,0 +1,43 @@
+import pybullet as p
+import time
+import pybullet_data
+
+
+p.connect(p.GUI)
+p.setGravity(0, 0, -9.8)
+
+# Default PyBullet assets
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+plane_id = p.loadURDF("plane.urdf")
+
+# Additional URDFs location
+p.setAdditionalSearchPath("/workspaces/pybullet_tutorial/demos/urdf")
+# fix base to world
+robot_id = p.loadURDF("rrbot.urdf", useFixedBase=True)
+
+# add slider for each joint
+joint_sliders = {}
+num_joints = p.getNumJoints(robot_id)
+for i in range(num_joints):
+    info = p.getJointInfo(robot_id, i)
+    j_name = info[1].decode("utf-8")
+    j_type = info[2]
+
+    slider_id = p.addUserDebugParameter(j_name, -3.14, 3.14, 0.0)
+    joint_sliders[i] = slider_id
+
+
+while p.isConnected():
+    for j, slider_id in joint_sliders.items():
+        target = p.readUserDebugParameter(slider_id)
+        # set joint position
+        p.setJointMotorControl2(
+            robot_id,
+            j,
+            controlMode=p.VELOCITY_CONTROL,
+            targetVelocity=target,
+            force=5.0
+        )
+    p.stepSimulation()
+    time.sleep(1./240.)
+
